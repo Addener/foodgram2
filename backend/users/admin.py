d0b2ebd1e3ae3086e2f8acfe91cb983742dc2b2b
Alecsei Uzhva <stas.basov@whatever.com>
2024-11-27 22:11:15 +0300

    change

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -29,5 +29,5 @@ class SubscriptionAdmin(admin.ModelAdmin):
 
     list_display = ['id', 'user', 'author']
     list_display_links = ['id', 'user']
-    search_fields = ('user', 'author',)
+    search_fields = ('author',)
     empty_value_display = 'Поле не заполнено'
